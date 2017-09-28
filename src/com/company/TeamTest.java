b6dd64d6c8585939f7f84ff598d727dba46cb881
ClaudeRenney <clauderenney@gmail.com>
2017-09-28 16:18:09 +0200

    "Added test class"

---
+++ src/com/company/TeamTest.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Claude on 9/28/17.
+ */
+public class TeamTest {
+    @Test
+    public void main() throws Exception {
+    }
+
+    @Test
+    public void go() throws Exception {
+    }
+
+    @Test
+    public void sorted() throws Exception {
+    }
+
+    @Test
+    public void readScores() throws Exception {
+    }
+
+    @Test
+    public void addScoresAndCalculateResults() throws Exception {
+    }
+
+    @Test
+    public void sortTableRows() throws Exception {
+    }
+
+}
