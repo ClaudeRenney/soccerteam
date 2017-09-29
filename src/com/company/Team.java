4028a34d6f27d1f0882df6e33618e63880172971
ClaudeRenney <clauderenney@gmail.com>
2017-09-29 08:09:10 +0200

    Added user input capabilities

--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -1,57 +1,96 @@
 package com.company;
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.*;
+
+import static java.lang.System.in;
 
 public class Team {
 
     HashMap<String, Integer> results = new HashMap<String, Integer>();
 
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+
 
 
 //        new Team().go();
-        new Team().sorted();
 
 
+         {
 
-    }
-
-
-    public void go() {
-
-        readScores(); // reading them in and create the results board
-
-        System.out.println(results);
+            new Team().sorted();
+        }
 
 
     }
-    public void sorted(){
+
+
+//    public void go() {
+//
+//        readScores(); // reading them in and create the results board
+//
+//        System.out.println(results);
+//
+//
+//    }
+
+    public void sorted() throws FileNotFoundException {
         readScores();
 
-        System.out.println(sortTableRows(results));
+            System.out.println(sortTableRows(results));
+        }
 
-    }
 
-    void readScores() {
-        try {
-            File file = new File("TeamScores.txt");
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line = null;
-            while ((line = reader.readLine()) != null) {
+
+
+
+
+
+
+void readScores() throws FileNotFoundException {
+
+    // Scanner for user input
+    Scanner user = new Scanner( in );
+    String  inputFileName;
+
+    // prepare the input file
+    try {
+    System.out.print("Input File Name: ");
+    inputFileName = user.nextLine().trim();
+    File file = new File( inputFileName );
+    Scanner scan = new Scanner( file );
+    BufferedReader reader = new BufferedReader(new FileReader(file));
+    String line;
+    while ((line = reader.readLine()) != null) {
                 addScoresAndCalculateResults(line);
-            }
+
+}
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+//    void readScores() {
+//        try {
+//
+//            File file = new File("TeamScores.txt");
+//            BufferedReader reader = new BufferedReader(new FileReader(file));
+//            String line = null;
+//            while ((line = reader.readLine()) != null) {
+//                addScoresAndCalculateResults(line);
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//    }
 
-    void addScoresAndCalculateResults(String lineToParse) { // add a club and score to arraylist rawscores
+    void addScoresAndCalculateResults(String lineToParse) {
+        // add a club and score to arraylist rawscores
         int totalscore;
+
         String[] match = lineToParse.split(","); // split the 2 clubs that played into match[0] and match[1]
         String[] clubscore1 = match[0].split(" "); // now split the first club name, clubscore1[0] and score, clubscore1[1]
         if (!results.containsKey(clubscore1[0])) { // only the first time will it be initialised with 0
@@ -86,15 +125,20 @@ public class Team {
     }
 
     public TreeMap<String, Integer> sortTableRows(Map<String, Integer> results) {
+
         CompareValues compareValues = new CompareValues(results);
 
         TreeMap<String, Integer> sortedMap = new TreeMap<>(compareValues);
         sortedMap.putAll(results);
 
-        sortedMap.forEach((k,v) -> System.out.println(k + "=" + v + " "));
-        return sortedMap;
 
-    }
+
+            sortedMap.forEach((k, v) -> System.out.println(k + "=" + v + " "));
+
+
+            return sortedMap;
+
+        }
 }
 
 
@@ -102,6 +146,7 @@ public class Team {
     class CompareValues implements Comparator<String> {
 
         Map<String, Integer> results;
+
 
         public CompareValues(Map<String, Integer> base) {
             this.results = base;
